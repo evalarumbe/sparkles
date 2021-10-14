9f220d01ea60881d97d9ac48dffa84589d94968d
Eva Larumbe <evalarumbe@gmail.com>
2021-10-14 11:22:38 +0200

    Refactor: Define the Sparkle class

--- index.js
+++ index.js
@@ -12,40 +12,116 @@
     }
     const { randomX, randomY, randomRadius, randomDelay } = utils;
     
+    class Sparkle {
+        constructor() {
+            this.x = randomX(); // center point's pixels from left
+            this.y = randomY(); // center point's pixels from top
+            this.rMin = randomRadius();
+            this.rMax = this.rMin * 2;
+            this.r = this.rMin; // animated. radius (height and width) of the sparkle
+            this.opacityMin = 0;
+            this.opacityMax = 0.7;
+            this.opacity = this.opacityMin; // animated
+        }
+
+        /**
+         * Set ending animation state
+         */
+        setEndState() {
+            gsap.to(this, {
+                duration: 2,
+                delay: firstRender ? randomDelay() : 0,
+                repeat: -1,
+                yoyo: true,
+                opacity: this.opacityMax,
+                r: this.rMax,
+            });
+        }
+
+        /**
+         * Draw a sparkle to the canvas
+         * Call draw in between calls to ctx.fillStyle(...) and ctx.fill().
+         */
+        draw() {
+            // vars for legibility
+            const { x, y, r } = this;
+            const halfR = r / 2;
+    
+            ctx.beginPath();
+            ctx.moveTo(x, (y - r)); // top point
+            ctx.bezierCurveTo(x, (y - halfR), (x + halfR), y, (x + r), y); // right point
+            ctx.bezierCurveTo((x + halfR), y, x, (y + halfR), x, (y + r)); // bottom point
+            ctx.bezierCurveTo(x, (y + halfR), (x - halfR), y, (x - r), y); // left point
+            ctx.bezierCurveTo((x - halfR), y, x, (y - halfR), x, (y - r)); // close
+            ctx.closePath();
+        }
+
+        /**
+         * Render the sparkle at its current opacity and size
+         */
+        render() {
+            const { r, rMin, rMax } = this;
+            ctx.fillStyle = `rgba(255, 255, 255, ${this.opacity})`;
+            this.draw();
+            ctx.fill();
+        }
+        
+        /**
+         * Test if a sparkle's center point is within a 200px radius of the cursor
+         * 
+         * Thank you Internet for saving me time on circle math
+         * https://www.w3resource.com/javascript-exercises/javascript-basic-exercise-120.php
+         * 
+         * @param {Event} - mousemove { offsetX, offsetY } cursor position
+         * @returns {Boolean}
+         */
+        isNearCursor(event) {
+            const { offsetX, offsetY } = event;
+            const radiusFromCursor = 200; // px
+            const distance = (this.x - offsetX) * (this.x - offsetX) + (this.y - offsetY) * (this.y - offsetY);
+            const rSquared = radiusFromCursor * radiusFromCursor;
+            return distance < rSquared;
+        }
+
+        /**
+         * Increase radius of a sparkle (affects the end state of animation)
+         * TODO: Consider making this chainable. See usage at handleMouseMove()
+         */
+        brighten() {
+            this.rMax = this.rMin * 4;
+            this.opacityMax = 1;
+        }
+        
+        /**
+         * Undo the effect of the brighten function
+         * TODO: Consider making this chainable. See usage at handleMouseMove()
+         */
+        dim() {
+            this.rMax = this.rMin * 2;
+            this.opacityMax = 0.7;
+        }
+    }
+
     const canvas = document.querySelector('#make-me-sparkle');
     const ctx = canvas.getContext('2d');
     let firstRender = true; // prevents a delay on resize
     const allSparkles = createSparkles(100);
 
-    // Fill the viewport with sparkles and start over on resize
-    initSparkles();
+    window.addEventListener('load', initSparkles);
     window.addEventListener('resize', handleResize);
     window.addEventListener('mousemove', handleMouseMove);
 
     /**
-     * Set starting properties for all sparkles, and how they will animate.
+     * Create the specified number of sparkles.
      * @param {number} n - How many sparkles would you like?
-     * @return {Array} sparkle objects
+     * @return {Array} Sparkle instances
      */
      function createSparkles(n) {
         const sparkles = [];
-        
-        // Generate the specified number of sparkles
         for (let i = 0; i < n; i++) {
-            const sparkle = {
-                x: randomX(),
-                y: randomY(),
-                rMin: randomRadius(),
-                rMax: this.rMin * 2,
-                r: this.rMin, // animated
-                opacityMin: 0,
-                opacityMax: 0.7,
-                opacity: this.opacityMin, // animated
-            };
-            
+            const sparkle = new Sparkle();            
             sparkles.push(sparkle);
         }
-        
         return sparkles;
     }
 
@@ -56,7 +132,7 @@
         // TODO: maybe canvas size should be handled separately, for readability
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
-        allSparkles.forEach(setEndState);
+        allSparkles.forEach(sparkle => sparkle.setEndState());
         animateSparkles(allSparkles);
     }
     
@@ -72,90 +148,36 @@
     }
 
     /**
-     * TODO: Doc
-     * @param {Event} event 
-     * @returns 
+     * Apply effect to nearby sparkles. Remove effect from other (distant) sparkles.
+     * @param {Event} mousemove
      */
     function handleMouseMove(event) {
         const brighterSparkles = sparklesNearCursor(event);
         const otherSparkles = allSparkles.filter(sparkle => !brighterSparkles.includes(sparkle));
         
         brighterSparkles.forEach(sparkle => {
-            brighten(sparkle);
-            setEndState(sparkle);
+            sparkle.brighten();
+            sparkle.setEndState();
         });
         
         otherSparkles.forEach(sparkle => {
-            dim(sparkle);
-            setEndState(sparkle);
+            sparkle.dim();
+            sparkle.setEndState();
         });
-    }
-
-    /**
-     * Increase radius of a sparkle (affects the end state of animation)
-     * @param {Object} - sparkle
-     */
-    function brighten(sparkle) {
-        sparkle.rMax = sparkle.rMin * 4;
-        sparkle.opacityMax = 1;
-    }
-    
-    /**
-     * Undo the effect of the brighten function
-     * @param {Object} - sparkle
-     */
-    function dim(sparkle) {
-        sparkle.rMax = sparkle.rMin * 2;
-        sparkle.opacityMax = 0.7;
     }
     
      /**
      * Identify sparkles that are close to the cursor
      * @param {Event} - { offsetX, offsetY } cursor position
-     * @returns {Array} - sparkles TODO: document these objects better up top
+     * @returns {Array} Sparkle instances
      */
     function sparklesNearCursor(event) {
-        const sparklesNearCursor = allSparkles.filter(sparkle => {
-            return isNearCursor(sparkle, event);
-        });
-        return sparklesNearCursor;
-    }
-
-    /**
-     * Test if a sparkle's center point is within a 200px radius of the cursor
-     * 
-     * Thank you Internet for saving me time on circle math
-     * https://www.w3resource.com/javascript-exercises/javascript-basic-exercise-120.php
-     * 
-     * @param {Object} - sparkle { x, y } center point
-     * @param {Event} - mousemove { offsetX, offsetY } cursor position
-     * @returns {Boolean}
-     */
-    function isNearCursor({ x, y }, { offsetX, offsetY }) {
-        const radiusFromCursor = 200; // px
-        const dist = (x - offsetX) * (x - offsetX) + (y - offsetY) * (y - offsetY);
-        const rSquared = radiusFromCursor * radiusFromCursor;
-        return dist < rSquared;
-    }
-
-    /**
-     * Set ending animation states for a set (or subset) of sparkles
-     * @param {Array} sparkles Which sparkles should this affect?
-     */
-    function setEndState(sparkle) {
-        gsap.to(sparkle, {
-            duration: 2,
-            delay: firstRender ? randomDelay() : 0,
-            repeat: -1,
-            yoyo: true,
-            opacity: sparkle.opacityMax,
-            r: sparkle.rMax,
-        });
+        return allSparkles.filter(sparkle => sparkle.isNearCursor(event));
     }
     
     /**
      * Continuously clear and redraw sparkles
-     * @param {Array} sparkles TODO: which properties do we need
+     * @param {Array} Sparkle instances
      */
     function animateSparkles(sparkles) {
         // On each frame (between animation states)
@@ -164,41 +186,8 @@
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             console.log('cleared');
             // Render all sparkles
-            sparkles.forEach(sparkle => {
-                renderSparkle(sparkle);
-            });
+            sparkles.forEach(sparkle => sparkle.render());
         });   
         firstRender = false;
-    }
-
-    /**
-     * Render the sparkle at its current opacity and size
-     * @param {{ opacity, x, y, r }} sparkle
-     * // TODO: JS Docs for object prop data types
-     */
-    function renderSparkle({ opacity, x, y, r }) {
-        ctx.fillStyle = `rgba(255, 255, 255, ${opacity})`;
-        drawSparkle(x, y, r);
-        ctx.fill();
-    }
-        
-    /**
-     * Draw a sparkle for a given radius and coordinate (x, y) position.
-     * Call drawSparkle in between calls to ctx.fillStyle(...) and ctx.fill().
-     * 
-     * @param {number} x - center point's pixels from left
-     * @param {number} y - center point's pixels from top
-     * @param {number} r - height and width (loosely, "radius") of the sparkle
-     */
-    function drawSparkle(x, y, r) {
-        const halfR = r / 2; // for legibility
-
-        ctx.beginPath();
-        ctx.moveTo(x, (y - r)); // top point
-        ctx.bezierCurveTo(x, (y - halfR), (x + halfR), y, (x + r), y); // right point
-        ctx.bezierCurveTo((x + halfR), y, x, (y + halfR), x, (y + r)); // bottom point
-        ctx.bezierCurveTo(x, (y + halfR), (x - halfR), y, (x - r), y); // left point
-        ctx.bezierCurveTo((x - halfR), y, x, (y - halfR), x, (y - r)); // close
-        ctx.closePath();
     }
 })();
